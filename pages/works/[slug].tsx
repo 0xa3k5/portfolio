7236bff694db9e1d1bc2be92cbd4d2f141397527
ak <a.kemalakcay@gmail.com>
2022-08-25 17:32:32 +0300

    add FeedbackCard

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import NotionService from '../api/notion';
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { GetStaticProps } from 'next';
 import ReactMarkdown from 'react-markdown';
 import Header from '../../src/components/Header';
 import Footer from '../../src/components/Footer';
@@ -10,34 +10,45 @@ import OverviewCard from '../../src/components/OverviewCard';
 import CTA from '../../src/components/CTA';
 import { getMorePosts } from '../../src/utils/getMorePosts';
 import MorePosts from '../../src/components/MorePosts';
+import { NotionPost, Feedback } from '../../@types/schema';
 
-const Detail = ({
-  markdown,
-  post,
-  morePosts,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+import Util from 'util';
+import FeedbackCard from '../../src/components/FeedbackCard';
+
+interface DetailProps {
+  markdown: string;
+  post: NotionPost;
+  morePosts: NotionPost[];
+  feedbacks: Feedback[];
+}
+
+const Detail = ({ markdown, post, morePosts, feedbacks }: DetailProps) => {
+  const postFeedbacks = feedbacks.filter((f) =>
+    post.feedbacks.relationIds.includes(f.id)
+  );
+
   return (
     <>
       <Head>
-        <title>{post.title}</title>
+        <title>{post.details.title}</title>
         <meta
           name='description'
           title='description'
-          content={post.description}
+          content={post.details.description}
         />
         <meta
           name='og:description'
           title='og:description'
-          content={post.description}
+          content={post.details.description}
         />
-        <meta name='og:image' title='og:title' content={post.img} />
+        <meta name='og:image' title='og:title' content={post.details.img} />
       </Head>
       <main className='bg-midnight'>
         <div
           className=''
           style={{
-            backgroundColor: `#${post.bgColor}`,
-            color: `#${post.color}`,
+            backgroundColor: `#${post.properties.bgColor}`,
+            color: `#${post.properties.color}`,
           }}
         >
           <div className='container flex h-screen flex-col justify-between'>
@@ -48,6 +59,7 @@ const Detail = ({
         </div>
         <div className='container flex flex-col items-center space-y-24 py-24 px-8 md:px-24'>
           <OverviewCard post={post} />
+
           <div className='mx-auto'>
             <div className='flex items-center justify-center'>
               <article className='with-prose'>
@@ -55,6 +67,7 @@ const Detail = ({
               </article>
             </div>
           </div>
+          <FeedbackCard classname='w-11/12' feedback={postFeedbacks} />
           <MorePosts posts={morePosts} />
         </div>
         <CTA />
@@ -69,6 +82,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const notionService = new NotionService();
 
+  const feedbacks = await notionService.getFeedbacks();
+
   const p = await notionService.getNotionPageDetail(
     context.params?.slug as string,
     config.notion.portfolioPosts
@@ -79,6 +94,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
       markdown: p.markdown,
       post: p.post,
       morePosts: pages.morePosts,
+      feedbacks: feedbacks,
     },
   };
 };
@@ -89,7 +105,7 @@ export async function getStaticPaths() {
   const posts = await notionService.getPortfolioPosts();
 
   const paths = posts.map((p) => {
-    return `/works/${p.slug}`;
+    return `/works/${p.properties.slug}`;
   });
 
   return {
---
+++ src/components/FeedbackCard/FeedbackCard.tsx
@@ -0,0 +1,68 @@
+import cx from 'classnames';
+import { Feedback } from '../../../@types/schema';
+
+import Image from 'next/image';
+import { useState } from 'react';
+
+interface FeedbackCardProps {
+  classname?: string;
+  feedback: Feedback[];
+}
+
+export default function FeedbackCard({
+  classname,
+  feedback,
+}: FeedbackCardProps): JSX.Element {
+  const [selected, setSelected] = useState<Feedback>(feedback[0]);
+
+  return (
+    <div
+      className={cx(
+        classname,
+        'container flex flex-col items-center space-y-12 overflow-hidden rounded-2xl bg-white bg-opacity-5 p-16'
+      )}
+    >
+      <div className='flex h-16 items-center space-x-8'>
+        {feedback.map((f) => {
+          return (
+            <div
+              className={cx(
+                'relative overflow-hidden rounded-full duration-200',
+                selected === f
+                  ? 'h-16 w-16 opacity-100'
+                  : 'h-12 w-12 opacity-40'
+              )}
+              key={f.id}
+              onClick={() => setSelected(f)}
+            >
+              <Image src={f.img} alt={f.name} layout='fill' objectFit='cover' />
+            </div>
+          );
+        })}
+      </div>
+      <div className='flex'>
+        {feedback.map((f) => {
+          return (
+            <div
+              className={cx(
+                selected === f ? 'visible' : 'hidden',
+                'flex h-full max-w-xl flex-col items-center space-y-8 text-center duration-200'
+              )}
+              key={f.id}
+            >
+              <span className='font-bogart text-2xl font-normal'>
+                {f.feedback}
+              </span>
+              <div className='flex flex-col space-y-2 border-t border-t-white border-opacity-20 pt-8'>
+                <span className='text-xl'>{f.name}</span>
+                <span className='opacity-40'>
+                  {f.role} @ {f.orgName}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
