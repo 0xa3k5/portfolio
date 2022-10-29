ed1ee2589b3eda3d1ed9696634b42cb5e9b7bfa1
ak <a.kemalakcay@gmail.com>
2022-10-29 11:43:41 +0300

    use `newTabButton` on CTA

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -1,4 +1,6 @@
-import cx from 'classnames';
+import cx from "classnames";
+import Button from "../Button";
+import { useState } from "react";
 
 interface CTAProps {
   className?: string;
@@ -9,20 +11,28 @@ interface CTAProps {
 export default function CTA({
   className,
   title = "Let's build something together!",
-  desc = 'I help startups with product design, design critics, QA testing and design advisory.',
+  desc = "I help startups with product design, design critics, QA testing and design advisory.",
 }: CTAProps): JSX.Element {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <div
       className={cx(
         className,
-        'flex h-[70vh] items-center justify-center px-4 md:px-0'
+        "flex h-[40vh] items-center justify-center px-4 md:h-[60vh] md:px-0"
       )}
     >
-      <div className='flex flex-col justify-center space-y-6 md:items-center md:space-y-8 md:text-center'>
-        <h4 className='max-w-xl font-vollkorn text-4xl font-semibold md:text-5xl'>
+      <div className="flex flex-col items-center justify-center space-y-6 text-center md:space-y-8">
+        <h4 className="max-w-xl font-vollkorn text-4xl font-semibold md:text-5xl">
           {title}
         </h4>
-        <p className='max-w-sm text-xl font-normal opacity-80'>{desc}</p>
+        <p className="max-w-sm text-xl font-normal opacity-80">{desc}</p>
+        <div
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <Button.NewTab hovered={hovered} href="mailto:hey@akml.io" />
+        </div>
       </div>
     </div>
   );
