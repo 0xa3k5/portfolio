a0be3eca33fc3b26dd6b3614e95b31f53b154b37
ak <a.kemalakcay@gmail.com>
2022-11-01 00:16:59 +0300

    fix login page deprecated dependencies

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,11 @@
 import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import { useState } from "react";
 import Logout from "../src/components/Form/Logout";
 import Header from "../src/components/Header/Header";
 import MobileMenu from "../src/components/Header/MobileMenu";
 import Login from "../src/components/Form/Login";
 
 const LoginPage = (): JSX.Element => {
-  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const { status } = useSession();
 
   return (
@@ -20,17 +18,14 @@ const LoginPage = (): JSX.Element => {
           content="this is a page to log in or log out"
         />
       </Head>
-      <Header isNavbarOpen={isNavbarOpen} setIsNavbarOpen={setIsNavbarOpen} />
-      <MobileMenu
-        isNavbarOpen={isNavbarOpen}
-        setIsNavbarOpen={setIsNavbarOpen}
-      />
+      <Header />
+      <MobileMenu />
       {status === "unauthenticated" ? (
         <Login redirectPath="/" />
       ) : (
         <Logout onClick={() => signOut()} />
       )}
-    </>
+  </>
   );
 };
 
