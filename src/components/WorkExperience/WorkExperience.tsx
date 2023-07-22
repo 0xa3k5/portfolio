63cc23575410aef52137f5ff78afd795c2e22f85
ak <a.kemalakcay@gmail.com>
2023-07-22 09:52:02 +0300

    fix: remove logo and company description from work experiences

--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -1,6 +1,4 @@
 import { Collaborator, WorkExp } from "../../types";
-
-import Image from "next/image";
 import Link from "next/link";
 import { useTheme } from "../../contexts/ThemeContext";
 import CollaboratorItem from "../Cards/OverviewCard/CollaboratorItem";
@@ -30,24 +28,6 @@ export default function WorkExperience({
           rel="noreferrer"
           className="group flex w-full flex-row items-center gap-4"
         >
-          {/* <span
-            className="relative h-12 w-12 shrink-0 overflow-clip rounded-lg duration-150 group-hover:-translate-y-1"
-            key={job.id}
-          >
-            <Image
-              src={job.logo}
-              alt={`${job.logo} logo`}
-              fill
-              priority
-              style={{
-                objectFit: "contain",
-                maxWidth: "100%",
-              }}
-              sizes="(max-width: 768px) 100vw,
-            (max-width: 1200px) 50vw,
-            33vw"
-            />
-          </span> */}
           <h6 className="text-2xl">{job.company}</h6>
         </Link>
       </div>
@@ -65,53 +45,3 @@ export default function WorkExperience({
     </div>
   );
 }
-
-// <div
-//       className={`flex ${themeClasses.border} flex-col-reverse items-center justify-between gap-4 border-b border-opacity-10 py-8 last-of-type:border-none sm:flex-row`}
-//     >
-//       <div className="flex flex-col gap-4">
-//         <span className={`${themeClasses.color} text-opacity-40`}>
-//           {job.period}
-//         </span>
-//         <Link
-//           href={job.website}
-//           target="_blank"
-//           rel="noreferrer"
-//           className="group flex w-full flex-row items-center gap-4"
-//         >
-//           <span
-//             className="relative h-12 w-12 shrink-0 overflow-clip rounded-lg duration-150 group-hover:-translate-y-1"
-//             key={job.id}
-//           >
-//             <Image
-//               src={job.logo}
-//               alt={`${job.logo} logo`}
-//               fill
-//               priority
-//               style={{
-//                 objectFit: "contain",
-//                 maxWidth: "100%",
-//               }}
-//               sizes="(max-width: 768px) 100vw,
-//             (max-width: 1200px) 50vw,
-//             33vw"
-//             />
-//           </span>
-//           <span className="flex w-full flex-col duration-150">
-//             <span className="text-lg">{job.company}</span>
-//             <span className="opacity-40">{job.tagline}</span>
-//           </span>
-//         </Link>
-//       </div>
-//       <ul className="grid grid-cols-6 gap-2">
-//         {collaborators.map((collab, i) => {
-//           return (
-//             <CollaboratorItem
-//               className="col-span-1"
-//               key={i}
-//               collaborator={collab}
-//             />
-//           );
-//         })}
-//       </ul>
-//     </div>
