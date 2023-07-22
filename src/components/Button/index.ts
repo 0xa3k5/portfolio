0a2a23a377836d8e66cb1e3db7a9e6369a6708ac
ak <a.kemalakcay@gmail.com>
2023-07-22 09:49:55 +0300

    feat: create email-copy-button

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -2,7 +2,8 @@ import Primary from "./Primary";
 import LinkBtn from "./LinkBtn";
 import Icon from "./Icon";
 import HireButton from "./HireButton";
+import Copy from './Copy';
 
-const variants = { Primary, LinkBtn, Icon, HireButton };
+const variants = { Primary, LinkBtn, Icon, HireButton, Copy };
 
 export default variants;
--- src/components/Hero/Home/index.tsx
+++ src/components/Hero/Home/index.tsx
@@ -4,12 +4,12 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import SocialButtonGroup from "../../SocialButtonGroup";
 import { Code, Rectangle, SideProjects, Crypto } from "./Items";
+import Button from "../../Button";
 
 export default function Hero(): JSX.Element {
   const { themeClasses, themeColors } = useTheme();
-
   return (
-    <div className="flex w-full flex-col gap-8 px-4 py-8 md:px-12 md:py-16">
+    <div className="flex w-full max-w-5xl flex-col gap-8 px-4 py-8 md:py-16">
       <div className="flex items-center gap-4">
         <div className="relative flex aspect-square w-14 shrink-0 overflow-clip rounded-xl">
           <Image
@@ -26,6 +26,7 @@ export default function Hero(): JSX.Element {
           <span className="opacity-60">Generalist Product Designer</span>
         </div>
       </div>
+
       <motion.h1
         layout
         className={cx(
@@ -42,7 +43,10 @@ export default function Hero(): JSX.Element {
         <span className="">{"– trade "}</span>
         <Crypto themeColors={themeColors} />
       </motion.h1>
-      <SocialButtonGroup />
+      <div className="flex gap-4">
+        <Button.Copy />
+        <SocialButtonGroup />
+      </div>
     </div>
   );
 }
